0cb0e2bace861fa420e61c32bfe666755e95d0f9
HikmatilloToshtemirov <toshtemirovhikmatilloh>
2022-07-06 07:55:22 +0500

    comment qoshdim 1

--- index.js
+++ index.js
@@ -20,18 +20,19 @@
 // shablon.zakazol.call(shablon1);
 // shablon.zakazol.call(shablon2);
 // shablon2.zakazol.call(shablon);
-const btn = document.getElementById("btn");
-const btn2 = document.getElementById("btn2");
-const btn3 = document.getElementById("btn3");
+// //
+// const btn = document.getElementById("btn");
+// const btn2 = document.getElementById("btn2");
+// const btn3 = document.getElementById("btn3");
 
-btn.addEventListener("click", () => {
-  console.log(`Zakaz qabul qilindi ( brend nomi:Fendi)`);
-});
+// btn.addEventListener("click", () => {
+//   console.log(`Zakaz qabul qilindi ( brend nomi: "Fendi")`);
+// });
 
-btn2.addEventListener("click", () => {
-  console.log(`Zakaz qabul qilindi ( brend nomi:Gucci)`);
-});
+// btn2.addEventListener("click", () => {
+//   console.log(`Zakaz qabul qilindi ( brend nomi: "Gucci")`);
+// });
 
-btn3.addEventListener("click", () => {
-  console.log(`Zakaz qabul qilindi ( brend nomi: "Zara")`);
-});
+// btn3.addEventListener("click", () => {
+//   console.log(`Zakaz qabul qilindi ( brend nomi: "Zara")`);
+// });
